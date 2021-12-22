0b0aeb5ce57c0fa10ada76060c09002d416c75de
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-12-22 11:47:06 -0800

    Update about.asm

--- bin/about/about.asm
+++ bin/about/about.asm
@@ -1,8 +1,9 @@
 %include A:/src/Crylix-main/kernel/sys_calls/print.asm
 about:
   push A
-  about_string:     data "CrylonOS 0.1-alpha18 \nyou are using a dev version of CrylonOS"
+  about_string:     data "RubixOS 0.1.0 \nyou are using a dev version of Rubix"
   about_string_two:       data "made by DiamondCoder1000 \ninspired by Robotman2412"
+  
   loadi A, about_string
   call print
   loadi A, about_string_two
